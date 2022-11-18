b2136bf83570ea0adbc63e07c6ad87652ddf8b7c
Kharatian <kharatian_arthur@mail.ru>
2022-11-18 20:21:38 +0300

    fix ....

--- src/itmo/java/basics/lesson2/ClassForGitHub.java
+++ src/itmo/java/basics/lesson2/ClassForGitHub.java
@@ -3,6 +3,6 @@ package itmo.java.basics.lesson2;
 public class ClassForGitHub {
 
     public static void main(String[] args) {
-        System.out.println("GitHub!");
+        System.out.println("OK!");
     }
 }
